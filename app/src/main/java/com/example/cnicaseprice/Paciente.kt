6ab89800bd05498da6e46a8a307464be7cccb5da
Santiago Rubio <santiago@codingit.dev>
2024-06-10 23:38:16 -0300

    feat: add patient table & connect data on register

--- app/src/main/java/com/example/cnicaseprice/Paciente.kt
+++ app/src/main/java/com/example/cnicaseprice/Paciente.kt
@@ -1,6 +1,4 @@
 package com.example.cnicaseprice
 
-import java.io.Serializable
-
 class Paciente(val DNI: String, val OS: String, val Nombre: String, val Apellido: String) {
 }
--- app/src/main/java/com/example/cnicaseprice/PatientActivity.kt
+++ app/src/main/java/com/example/cnicaseprice/PatientActivity.kt
@@ -1,13 +1,12 @@
 package com.example.cnicaseprice
 
-import android.content.Intent
+
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
 
 class PatientActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
