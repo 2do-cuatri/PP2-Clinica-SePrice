2b37d6d3624ce1c17f61c2a926faaf8884e4d684
Santiago Javier Rubio <s.rubio91@gmail.com>
2024-06-15 23:40:52 -0300

    refactor: saque los componentes innecesarios

--- app/src/main/java/com/example/cnicaseprice/AdminMenuActivity.kt
+++ app/src/main/java/com/example/cnicaseprice/AdminMenuActivity.kt
@@ -26,15 +26,10 @@ class AdminMenuActivity : AppCompatActivity() {
             val intent3 = Intent(this, StudyVoucherActivity::class.java)
             startActivity(intent3)
         }
-        val button4 = findViewById<Button>(R.id.btnClinic)
+        val button4 = findViewById<Button>(R.id.btnBackMain)
         button4.setOnClickListener{
-            val intent4 = Intent(this, ClinicActivity::class.java)
+            val intent4 = Intent(this, MainActivity::class.java)
             startActivity(intent4)
-        }
-        val button5 = findViewById<Button>(R.id.btnBackMain)
-        button5.setOnClickListener{
-            val intent5 = Intent(this, MainActivity::class.java)
-            startActivity(intent5)
         }
     }
 }
